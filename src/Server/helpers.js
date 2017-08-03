2bfb7925d13c9e0c2ca5f49a0c071e6388d5169f
bruno ziiê <eu@brunoziie.com>
2017-08-03 20:26:22 -0300

    create middleware to call beforeRouteAction method

--- src/Server/helpers.js
+++ src/Server/helpers.js
@@ -25,7 +25,17 @@ helpers.makeMiddlewareChain = function (middleware, finalHandler, isGlobal, reso
     return middleware.resolve([], true).concat([{instance: null, method: finalHandler}])
   }
   const routeMiddleware = middleware.resolve(middleware.formatNamedMiddleware(resolvedRoute.middlewares), false)
-  return routeMiddleware.concat([finalHandler])
+  
+  let beforeRouteActionMiddleware = function * (request, response, next) {
+    // Inject request and response into controller instance
+    if (finalHandler.instance && finalHandler.instance.beforeRouteAction) {
+      finalHandler.instance.beforeRouteAction(request, response)
+    }
+
+    yield next
+  }
+
+  return routeMiddleware.concat([beforeRouteActionMiddleware, finalHandler])
 }
 
 /**
